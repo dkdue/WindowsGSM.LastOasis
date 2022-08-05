aef056cb58fcff23a0e298b00b84d9198cb452d6
Jamie96ITS <26676527+Jamie96ITS@users.noreply.github.com>
2022-08-05 13:44:41 -0400

    Renamed StartPath EXE.
    In the last update, they renamed `MistServer-Win64-Shipping.exe` to `MistServer.exe`. Unclear if this will be reverted but here is the change.

--- LastOasis.cs/LastOasis.cs
+++ LastOasis.cs/LastOasis.cs
@@ -34,7 +34,7 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"Mist\Binaries\Win64\MistServer-Win64-Shipping.exe"; // Game server start path
+        public override string StartPath => @"Mist\Binaries\Win64\MistServer.exe"; // Game server start path
         public string FullName = "LastOasis Dedicated Server"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
